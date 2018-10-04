8e6f40b71ad662759ecfed339a3f38c706e31e3f
yann <yann.lanuel@univ-lorraine.fr>
2018-10-04 11:01:22 +0200

    Ajout d'image dans les boutons (non résolu)

--- 2018-2019/L2 INFO/BPOO/ProjetBPOO2018-2019/src/ui/CandyCrush.java
+++ 2018-2019/L2 INFO/BPOO/ProjetBPOO2018-2019/src/ui/CandyCrush.java
@@ -1,10 +1,12 @@
 package ui;
 
 import javafx.application.Application;
-import javafx.event.Event;
+
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.ClipboardContent;
 import javafx.scene.input.DragEvent;
 import javafx.scene.input.Dragboard;
@@ -13,13 +15,84 @@ import javafx.scene.input.TransferMode;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-
 
 public class CandyCrush extends Application {
 
+	private Button[][] grille;
+	private GridPane grillePane;
 
+	private final class DragDetectedEvent implements EventHandler<MouseEvent> {
+		private final Button bouton;
+
+		private DragDetectedEvent(Button bouton) {
+			this.bouton = bouton;
+		}
+
+		public void handle(MouseEvent event) {
+			Dragboard db = bouton.startDragAndDrop(TransferMode.ANY);
+
+			db.setDragView(bouton.snapshot(null, null));
+
+			ClipboardContent content = new ClipboardContent();
+			content.putString("");
+			db.setContent(content);
+
+			event.consume();
+		}
+	}
+
+	private final class DragOverEvent implements EventHandler<DragEvent> {
+		private final Button bouton;
+
+		private DragOverEvent(Button bouton) {
+			this.bouton = bouton;
+		}
+
+		public void handle(DragEvent event) {
+			if (event.getGestureSource() != bouton) {
+				event.acceptTransferModes(TransferMode.COPY_OR_MOVE);
+			}
+
+			event.consume();
+		}
+	}
+
+	private final class DragDroppedEvent implements EventHandler<DragEvent> {
+		private final Button bouton;
+
+		private DragDroppedEvent(Button bouton) {
+			this.bouton = bouton;
+		}
+
+		public void handle(DragEvent event) {
+			echangerSourceTarget(event.getGestureSource(), event.getGestureTarget());
+			event.consume();
+		}
+
+		private void echangerSourceTarget(Object source, Object target) {
+			int ls=0, cs=0, lt=0, ct=0;
+
+			for (int l = 0; l < 10; l++) {
+				for (int c = 0; c < 10; c++) {
+					if (grille[l][c] == source) {
+						ls = l;
+						cs = c;
+					} else if (grille[l][c] == target) {
+						lt = l;
+						ct = c;
+					}
+
+				}
+			}
+			
+			grille[ls][cs] = (Button)target;
+			grille[lt][ct] = (Button)source;
+			
+			
+			recreerGridPane();
+		}
+	}
 
 	@Override
 	public void start(Stage primaryStage) {
@@ -30,8 +103,11 @@ public class CandyCrush extends Application {
 
 			initGrille(root);
 
-			Scene scene = new Scene(root, 400, 400);
+			Scene scene = new Scene(root, 280, 280);
+			
+
 			primaryStage.setScene(scene);
+	
 			primaryStage.show();
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -39,121 +115,56 @@ public class CandyCrush extends Application {
 	}
 
 	private void initGrille(Pane panneau) {
-		GridPane grille = new GridPane();
-		grille.setStyle("-fx-background-color: #D2691E");
-
+		
+		Image imageOk = null;
+		try {
+			imageOk = new Image(getClass().getResourceAsStream("Candy_4.png"));
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		grillePane = new GridPane();
+		
+		grille = new Button[10][10];
 
 		for (int l = 0; l < 10; l++) {
 			for (int c = 0; c < 10; c++) {
-				Button bouton = new Button("B");
-				if ((l+c) % 2 == 0)
+				Button bouton = new Button("B",new ImageView(imageOk));
+				
+				
+				
+				
+				if ((l + c) % 2 == 0)
 					bouton.setStyle("-fx-background-color: #D2691E");
 				else
 					bouton.setStyle("-fx-background-color: #FFD700");
-					
+				grille[l][c] = bouton;
+
 				GridPane.setConstraints(bouton, c, l);
-				grille.getChildren().add(bouton);
+				grillePane.getChildren().add(bouton);
 
-				
-				
-				bouton.setOnDragDetected(new EventHandler <MouseEvent>() {
-		            public void handle(MouseEvent event) {
-		                /* drag was detected, start drag-and-drop gesture*/
-		                System.out.println("onDragDetected");
-		                
-		                /* allow any transfer mode */
-		                Dragboard db = bouton.startDragAndDrop(TransferMode.ANY);
+				bouton.setOnDragDetected(new DragDetectedEvent(bouton));
+				bouton.setOnDragOver(new DragOverEvent(bouton));
+				bouton.setOnDragDropped(new DragDroppedEvent(bouton));
 
-		                // This is where the magic happens, you take a snapshot of the HBox.
-		                db.setDragView(bouton.snapshot(null, null));
-		                
-		                /* put a string on dragboard */
-		                ClipboardContent content = new ClipboardContent();
-		                content.putString(bouton.getText());
-		                db.setContent(content);
-		                
-		                event.consume();
-		            }
-		        });
+			}
+		}
 
-				bouton.setOnDragOver(new EventHandler <DragEvent>() {
-		            public void handle(DragEvent event) {
-		                /* data is dragged over the target */
-		                System.out.println("onDragOver");
-		                
-		                /* accept it only if it is  not dragged from the same node 
-		                 * and if it has a string data */
-		                if (event.getGestureSource() != bouton &&
-		                        event.getDragboard().hasString()) {
-		                    /* allow for both copying and moving, whatever user chooses */
-		                    event.acceptTransferModes(TransferMode.COPY_OR_MOVE);
-		                }
-		                
-		                event.consume();
-		            }
-		        });
+		panneau.getChildren().add(grillePane);
+	}
 
-				bouton.setOnDragEntered(new EventHandler <DragEvent>() {
-		            public void handle(DragEvent event) {
-		                /* the drag-and-drop gesture entered the target */
-		                System.out.println("onDragEntered");
-		                /* show to the user that it is an actual gesture target */
-		                if (event.getGestureSource() != bouton &&
-		                        event.getDragboard().hasString()) {
-	
-		                }
-		                
-		                event.consume();
-		            }
-		        });
-
-				bouton.setOnDragExited(new EventHandler <DragEvent>() {
-		            public void handle(DragEvent event) {
-		                /* mouse moved away, remove the graphical cues */
-	
-		                
-		                event.consume();
-		            }
-		        });
-		        
-		        bouton.setOnDragDropped(new EventHandler <DragEvent>() {
-		            public void handle(DragEvent event) {
-		                /* data dropped */
-		                System.out.println("onDragDropped");
-		                /* if there is a string data on dragboard, read it and use it */
-		                Dragboard db = event.getDragboard();
-		                boolean success = false;
-		                if (db.hasString()) {
-		                	bouton.setText(db.getString());
-		                    success = true;
-		                }
-		                /* let the source know whether the string was successfully 
-		                 * transferred and used */
-		                event.setDropCompleted(success);
-		                
-		                event.consume();
-		            }
-		        });
-
-		        bouton.setOnDragDone(new EventHandler <DragEvent>() {
-		            public void handle(DragEvent event) {
-		                /* the drag-and-drop gesture ended */
-		                System.out.println("onDragDone");
-		                /* if the data was successfully moved, clear it */
-		                if (event.getTransferMode() == TransferMode.MOVE) {
-		                	bouton.setText("");
-		                }
-		                
-		                event.consume();
-		            }
-		        });
-
-				
-				
+	private void recreerGridPane() {
+		grillePane.getChildren().clear();
+		
+		for (int l = 0; l < 10; l++) {
+			for (int c = 0; c < 10; c++) {
+				grillePane.add(grille[l][c], c, l);
 			}
 		}
 		
-		panneau.getChildren().add(grille);
+		grillePane.requestLayout();
 	}
 
 	public static void main(String[] args) {
