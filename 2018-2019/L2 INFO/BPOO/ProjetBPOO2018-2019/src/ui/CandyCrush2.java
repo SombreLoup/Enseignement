5452cc60aecde4925e02438615d3bb8b88c6ec17
yann <yann.lanuel@univ-lorraine.fr>
2018-10-09 07:47:32 +0200

    Tests de mise au point de l'animation

--- 2018-2019/L2 INFO/BPOO/ProjetBPOO2018-2019/src/ui/CandyCrush2.java
+++ 2018-2019/L2 INFO/BPOO/ProjetBPOO2018-2019/src/ui/CandyCrush2.java
@@ -26,66 +26,81 @@ import javafx.stage.WindowEvent;
 import javafx.util.Duration;
 import modele.Plateau;
 
-
-
 public class CandyCrush2 extends Application {
-
-
-
 
 	private static final int NOMBRE_DE_CANDIES = 5;
 	private Button[][] grille;
 	private Canvas grillePane;
 	private Image[] candies;
 	private Plateau plateau;
-	private int	etatAnimation = 0;
+	private int etatAnimation = 0;
 	private Timeline timeline;
-	private	Stage stage;
+	private Stage stage;
 	private VBox root;
 	private Scene scene;
 	private int xd, yd, xf, yf;
-	
-	
+
 	public final class EventVidage implements EventHandler<ActionEvent> {
 
 		@Override
 		public void handle(ActionEvent event) {
 			System.out.println("Vidage");
-			
+
 		}
-	}	
-	
+	}
+
 	public final class EventRemplissage implements EventHandler<ActionEvent> {
 
 		@Override
 		public void handle(ActionEvent event) {
-			System.out.println("Remplissage");	
+			System.out.println("Remplissage");
 			plateau.debugConsole();
 			plateau.eliminerCasesVides();
 			plateau.debugConsole();
-	
+
 		}
-		
+
 	}
-	
 
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			this.stage = primaryStage;
 			primaryStage.setTitle("Candy Crush");
-	
+
 			initGrille(); // construction de grillePane
 			root = new VBox();
 			root.getChildren().add(grillePane);
 
 			scene = new Scene(root);
+			
+	        KeyFrame ViderCases = new KeyFrame(Duration.seconds(1),
+	                new EventHandler<ActionEvent>() {
+	        			public void handle(ActionEvent event) {
+	        				grillePane.getGraphicsContext2D().clearRect(0, 0, 640, 640);
+	        				plateau.eliminerCombos();
+	        				dessinerGrille();
+	        				System.out.println("Combos éliminés");
+
+	                    }
+	                });
+			
+	        KeyFrame RemplirCasesVides = new KeyFrame(Duration.seconds(1),
+	                new EventHandler<ActionEvent>() {
+	        			public void handle(ActionEvent event) {
+//	        				grillePane.getGraphicsContext2D().clearRect(0, 0, 640, 640);
+	        				plateau.eliminerCasesVides();
+	        				//dessinerGrille();
+	        				System.out.println("Remplissage fait");
+	        				timeline.stop();
+
+	                    }
+	                });
+	        
+	        timeline = new Timeline(ViderCases, RemplirCasesVides);
 
 			primaryStage.setScene(scene);
 			primaryStage.show();
-			
-
-
 
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -95,8 +110,8 @@ public class CandyCrush2 extends Application {
 	private void initGrille() {
 
 		initCandies();
-		grillePane = new Canvas(640,640);
-		
+		grillePane = new Canvas(640, 640);
+
 		grillePane.setOnDragDetected(new DragDetectedEvent());
 		grillePane.setOnDragOver(new DragOverEvent());
 		grillePane.setOnDragDropped(new DragDroppedEvent());
@@ -105,7 +120,7 @@ public class CandyCrush2 extends Application {
 
 		plateau = new Plateau(10, 10);
 		plateau.initPlateauAleatoire();
-		
+
 		dessinerGrille();
 	}
 
@@ -115,13 +130,12 @@ public class CandyCrush2 extends Application {
 		for (int l = 0; l < 10; l++) {
 			for (int c = 0; c < 10; c++) {
 				int indiceImage = plateau.getBonbon(l, c).getSorte().ordinal();
-				
-				gc.drawImage(candies[indiceImage], c*64, l*64 );
+
+				gc.drawImage(candies[indiceImage], c * 64, l * 64);
 
 			}
 		}
 	}
-
 
 	private void initCandies() {
 		candies = new Image[NOMBRE_DE_CANDIES];
@@ -142,13 +156,13 @@ public class CandyCrush2 extends Application {
 
 		public void handle(MouseEvent event) {
 			Dragboard db = grillePane.startDragAndDrop(TransferMode.ANY);
-			
-			xd = (int)event.getX();
+
+			xd = (int) event.getX();
 			yd = (int) event.getY();
 
-			int l = yd/64;
-			int c = xd/64;
-			
+			int l = yd / 64;
+			int c = xd / 64;
+
 			db.setDragView(candies[plateau.getBonbon(l, c).getSorte().ordinal()]);
 //			db.setDragView(grillePane.snapshot(null,null));
 
@@ -166,9 +180,9 @@ public class CandyCrush2 extends Application {
 		}
 
 		public void handle(DragEvent event) {
-			
-				event.acceptTransferModes(TransferMode.COPY_OR_MOVE);
-			
+
+			event.acceptTransferModes(TransferMode.COPY_OR_MOVE);
+
 			event.consume();
 		}
 	}
@@ -179,11 +193,13 @@ public class CandyCrush2 extends Application {
 
 		public void handle(DragEvent event) {
 
-			xf = (int)event.getX();
+			xf = (int) event.getX();
 			yf = (int) event.getY();
-			
-			echangerSourceTarget();
-			
+
+			Platform.runLater(() -> {
+				echangerSourceTarget();
+			});
+
 			event.consume();
 			System.out.println("Drag fini");
 		}
@@ -191,33 +207,38 @@ public class CandyCrush2 extends Application {
 		private void echangerSourceTarget() {
 			int ls = 0, cs = 0, lt = 0, ct = 0;
 
-			ls = yd/64;
-			cs = xd/64;
-			lt = yf/64;
-			ct = xf/64;
-			
-			System.out.println("ls="+ls+" cs="+cs+" lt="+lt+" ct="+ct);
+			ls = yd / 64;
+			cs = xd / 64;
+			lt = yf / 64;
+			ct = xf / 64;
+
+			System.out.println("ls=" + ls + " cs=" + cs + " lt=" + lt + " ct=" + ct);
 
 			plateau.echanger(ls, cs, lt, ct);
 
 			System.out.println("Echange fait");
 			
-			plateau.eliminerCombos();
-			dessinerGrille();
-			
-			System.out.println("Cases vides ?");
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			System.out.println("pause finie");
-//			plateau.eliminerCasesVides();
-//			dessinerGrille();
+			timeline.play();
+
+//			Platform.runLater(() -> {
+//				plateau.eliminerCombos();
+//				dessinerGrille();
+//				System.out.println("Cases vides ?");
+//				try {
+//					Thread.sleep(1000);
+//				} catch (InterruptedException e) {
+//					e.printStackTrace();
+//				}
+//			});
+//
+//			Platform.runLater(() -> {
+//				System.out.println("pause finie");
+//				plateau.eliminerCasesVides();
+//				//dessinerGrille();
+//			});
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
